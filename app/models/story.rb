9566add3c7a4312d9763fd7d5a06b1546ec282dc
Neville Samuell <nevilles@gmail.com>
2012-12-01 12:13:36 -0500

    Adding the user/story association to db

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,5 @@
 class Story < ActiveRecord::Base
+  belongs_to :user, inverse_of: :stories
+  validates_associated :user
   attr_accessible :body, :title
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :stories, inverse_of: :user
   attr_accessible :email, :name
 end
