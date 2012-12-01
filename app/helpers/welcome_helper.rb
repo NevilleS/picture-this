aef6722a359e4680772d2700007059d8bd2d3679
perry <perry.haldenby@gmail.com>
2012-12-01 17:02:03 -0500

    add fucntionality to fetch a photo with two users in it together

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -9,4 +9,37 @@ module WelcomeHelper
     @photo
   end
 
+  def getFriendshipPhoto (friendId1, friendId2, access_token, max_height, max_width)
+    
+    @graph = Koala::Facebook::API.new(access_token)
+    @access_token = access_token
+    query = "SELECT pid, src_big, src_big_height, src_big_width, caption FROM photo WHERE pid IN (SELECT pid FROM photo_tag WHERE subject = 122604676) AND pid IN (SELECT pid FROM photo_tag WHERE subject = 122603619)"
+    
+    @data = @graph.fql_query(query)
+    @rand_picture = @data[rand(@data.size)]
+
+    # Scale the image 
+    height = @rand_picture["src_big_height"]
+    width = @rand_picture["src_big_width"]
+
+    heightFactor = max_height * 1.0 / height
+    widthFactor = max_width * 1.0 / width
+
+    if heightFactor > widthFactor
+      puts widthFactor
+      factor = widthFactor
+    else
+      puts heightFactor
+      factor = heightFactor
+    end
+
+    newWidth = width * factor
+    newHeight = height * factor
+
+    puts @rand_picture["src_big"]
+
+    return_image_string = "<img src=\"" + @rand_picture["src_big"] + "\" width=\""+newWidth.to_s+"\" height=\""+newHeight.to_s+"\" />"
+    return_image_string
+  end
+
 end
