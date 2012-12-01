1926c7f3571d04159630520ed501ea4a414867df
Neville Samuell <nevilles@gmail.com>
2012-12-01 11:17:53 -0500

    Creating the PictureThis repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PictureThis::Application.config.secret_token = '97a5299c02e7e04ff63d07e5734a044e20e4e95ceca75000f851158d83d10026611fe92afd12605938382cc03531f74809742ea10dabd0cb03e53545f3a07c9e'
