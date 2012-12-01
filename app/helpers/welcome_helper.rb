b851968777fd0421cfc666d7e87d8c5c1b69a3e6
perry <perry.haldenby@gmail.com>
2012-12-01 17:20:55 -0500

    clean up

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,6 +1,6 @@
 module WelcomeHelper
 
-  def getFriendPic(friendId, access_token)
+  def getFriendProfilePhoto(friendId, access_token)
     @graph = Koala::Facebook::API.new(access_token)
     @access_token = access_token
     @user = @graph.get_object("me")
@@ -8,12 +8,24 @@ module WelcomeHelper
     @photo = @graph.get_picture(friendId, :type=>"large")
     @photo
   end
+ 
+  def getFriendPhoto(friendId, access_token, max_width, max_height)
+    @graph = Koala::Facebook::API.new(access_token)
+    @access_token = access_token
+    query = "SELECT pid, src_big, src_big_height, src_big_width, caption FROM photo WHERE pid IN (SELECT pid FROM photo_tag WHERE subject = 122604676)"
 
-  def getFriendshipPhoto (friendId1, friendId2, access_token, max_height, max_width)
+    @user = @graph.get_object("me")
+    @name = @user["name"]
+    @photo = @graph.get_picture(friendId, :type=>"large")
+    @photo
+  end
+
+
+  def getFriendshipPhoto(friendId1, friendId2, access_token, max_width, max_height)
     
     @graph = Koala::Facebook::API.new(access_token)
     @access_token = access_token
-    query = "SELECT pid, src_big, src_big_height, src_big_width, caption FROM photo WHERE pid IN (SELECT pid FROM photo_tag WHERE subject = 122604676) AND pid IN (SELECT pid FROM photo_tag WHERE subject = 122603619)"
+    query = "SELECT pid, src_big, src_big_height, src_big_width, caption FROM photo WHERE pid IN (SELECT pid FROM photo_tag WHERE subject = "+friendId1.to_s+") AND pid IN (SELECT pid FROM photo_tag WHERE subject = "+friendId2.to_s+")"
     
     @data = @graph.fql_query(query)
     @rand_picture = @data[rand(@data.size)]
