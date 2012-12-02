9655519419dc4ae5257f7cc4413ca482dc5d671e
jacalulu <jaclyn.konzelmann@gmail.com>
2012-12-01 23:48:40 -0500

    story page UI updates and background added to the enter information page

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,29 +10,40 @@ Template.create(
   title: "Epic Road Trip",
   body:
   %{
+    
+    <br/><br/><br/>
+    <div class="titlestory"> Picture this...</div><br/><br/>
     <div class="storyfont">
-    <br/><br/><br/><br/><br/><br/>
-    Picture this...<br/><br/><br/><br/><br/><br/>
-
-    <PT_IMAGE_1><br /><br/><br/><br/>
-    <PT_USER> and <PT_FRIEND_1> were packing up their <PT_VEHICLE_1> for their road trip to <PT_PLACE_1>.<br />
-    <br/><br/><br/><br/><br/><br/>
-
-    <PT_IMAGE_2><br /><br/><br/><br/>
-    When all of a sudden <PT_NUMBER_1> <PT_OBJECTS_1> fell from <PT_LOCATION_1>!<br />
-    <br/><br/><br/><br/><br/><br/>
-
-    <PT_IMAGE_4><br /><br/><br/><br/>
-    <PT_USER> and <PT_FRIEND_1> grabbed the nearest <PT_OBJECTS_2> and ran for shelter.<br />
-    <br/><br/><br/><br/><br/><br/>
-
-    <PT_IMAGE_3><br /><br/><br/><br/>
-    It looked like their road trip might not happen. Just as they were about to give up, <PT_FRIEND_2> showed up with <PT_OBJECTS_3> and saved the day!<br />
-    <br/><br/><br/><br/><br/><br/>
-
-    <PT_IMAGE_7><br /><br/><br/><br/>
-    Morale of the story: always remember to invite <PT_FRIEND_2> on trips, or pack your own <PT_OBJECTS_3>.<br />
-    <br/><br/><br/><br/><br/><br/>
+      <div class="imagecenter"><PT_IMAGE_1></div><br /><br/>
+    <div class="storyline1"> <div class="storyplotDiv">
+      <PT_USER> and <PT_FRIEND_1> were packing up their <PT_VEHICLE_1> for their road trip to <PT_PLACE_1>.<br />
+    </div></div>
+      <br/><br/>
+      <div class="imagecenter"><PT_IMAGE_2></div>
+      <br /><br/>
+    <div class="storyline2"> <div class="storyplotDiv">
+      When all of a sudden <PT_NUMBER_1> <PT_OBJECTS_1> fell from <PT_LOCATION_1>!<br />
+    </div></div>
+      <br/><br/>
+      <div class="imagecenter"><PT_IMAGE_4> </div> 
+      <br /><br/>
+    <div class="storyline3"> <div class="storyplotDiv">
+      <PT_USER> and <PT_FRIEND_1> grabbed the nearest <PT_OBJECTS_2> and ran for shelter.<br />
+    </div></div>
+      <br/><br/>
+      <div class="imagecenter"><PT_IMAGE_3></div>
+      <br /><br/>
+    <div class="storyline4"> <div class="storyplotDiv">
+      It looked like their road trip might not happen :( <br/> 
+      Just as they were about to give up, <PT_FRIEND_2> showed up with <PT_OBJECTS_3> and saved the day!<br />
+    </div></div>
+      <br/><br/>
+      <div class="imagecenter"><PT_IMAGE_7></div>
+      <br /><br/>
+    <div class="storyline5"> <div class="storyplotDiv">
+      MORALE OF THE STORY: <br/> always remember to invite <PT_FRIEND_2> on trips, or pack your own <PT_OBJECTS_3>.<br />
+    </div></div>
+    <br/><br/>
 
     </div>
   }
